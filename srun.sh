97f182c880ecdda329e1cf94e8f5a2ca66c52792
Amabel Tenghe <amabel.tenghe@helmholtz-muenchen.de>
2023-07-17 19:49:23 +0200

    add more scripts

---
+++ srun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Request resources for interactive sessioni
+# First check for nodes with free resources using: sinfo --state=idle,  then run one of the following:
+
+#srun -p cpu_p -q cpu_normal  --time=1-0:0:0 --mem=5G --pty /bin/bash
+
+srun -p interactive_cpu_p -q interactive_cpu --time=0-10:0:0 --mem=15G --pty /bin/ba
